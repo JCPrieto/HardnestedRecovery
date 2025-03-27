62405d27a123c53e22688192c4329e8e746cd6f0
Juan Carlos Prieto <JuanC.Prieto.Silos@gmail.com>
2025-03-27 22:18:05 +0100

    Volcar las claves descubiertas a un archivo.

--- hardnested_main.c
+++ hardnested_main.c
@@ -15,12 +15,12 @@ typedef enum {
 } key_type_t;
 
 // Function prototypes
-int process_nonces(FILE *fp, uint32_t uid, uint8_t sector, key_type_t key_type);
+int process_nonces(FILE *fp, uint32_t uid, uint8_t sector, key_type_t key_type, FILE *output_fp);
 int write_nonces_to_temp_file(FILE *fp, FILE *temp_fp, uint32_t current_uid, uint8_t current_sector, key_type_t current_key_type);
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <nonce_file_path>\n", argv[0]);
+    if (argc < 2 || argc > 4) {
+        printf("Usage: %s <nonce_file_path> [-o <output_file>]\n", argv[0]);
         return 1;
     }
 
@@ -28,6 +28,16 @@ int main(int argc, char *argv[]) {
     if (fp == NULL) {
         perror("Error opening file");
         return 1;
+    }
+
+    FILE *output_fp = NULL;
+    if (argc == 4 && (strcmp(argv[2], "-o") == 0 || strcmp(argv[2], "--output") == 0)) {
+        output_fp = fopen(argv[3], "w");
+        if (output_fp == NULL) {
+            perror("Error opening output file");
+            fclose(fp);
+            return 1;
+        }
     }
 
     uint32_t current_uid = 0;
@@ -57,7 +67,7 @@ int main(int argc, char *argv[]) {
             if (uid != current_uid || sector != current_sector || key_type != current_key_type) {
                 if (current_uid != 0) {
                     fseek(fp, 0, SEEK_SET);  // Seek to the beginning of the file
-                    process_nonces(fp, current_uid, current_sector, current_key_type);
+                    process_nonces(fp, current_uid, current_sector, current_key_type, output_fp);
                     fseek(fp, file_position, SEEK_SET);  // Return to the current position
                 }
                 current_uid = uid;
@@ -69,14 +79,17 @@ int main(int argc, char *argv[]) {
 
     if (current_uid != 0) {
         fseek(fp, 0, SEEK_SET);  // Seek to the beginning of the file
-        process_nonces(fp, current_uid, current_sector, current_key_type);
+        process_nonces(fp, current_uid, current_sector, current_key_type, output_fp);
     }
 
     fclose(fp);
+    if (output_fp != NULL) {
+        fclose(output_fp);
+    }
     return 0;
 }
 
-int process_nonces(FILE *fp, uint32_t uid, uint8_t sector, key_type_t key_type) {
+int process_nonces(FILE *fp, uint32_t uid, uint8_t sector, key_type_t key_type, FILE *output_fp) {
     char temp_file[] = "temp_nonces.txt";
     FILE *temp_fp = fopen(temp_file, "w");
     if (temp_fp == NULL) {
@@ -98,6 +111,11 @@ int process_nonces(FILE *fp, uint32_t uid, uint8_t sector, key_type_t key_type) 
     if (result == 1) {
         printf("Key found for UID: %08x, Sector: %d, Key type: %c: %012" PRIx64 "\n",
                uid, sector, (key_type == KEY_A) ? 'A' : 'B', foundkey);
+        if (output_fp != NULL) {
+            fprintf(output_fp, "Key found for UID: %08x, Sector: %d, Key type: %c: %012" PRIx64 "\n",
+                    uid, sector, (key_type == KEY_A) ? 'A' : 'B', foundkey);
+            fflush(output_fp); // Fuerza la escritura inmediata en el archivo
+        }
     } else {
         printf("Key not found for UID: %08x, Sector: %d, Key type: %c\n",
                uid, sector, (key_type == KEY_A) ? 'A' : 'B');
